b8369b3d6d72db89aace3debb3ef6f39d03eed6b
Esraa413 <esraaabdelwahab614@gmail.com>
2024-02-17 12:22:45 -0800

    web

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['52.200.66.208', '54.237.106.89']
 
 
 def do_deploy(archive_path):
